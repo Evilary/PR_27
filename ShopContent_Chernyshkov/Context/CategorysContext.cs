8a0276c1f1de18dc8a1f3635c186a8b7cb0a3bf7
df275 <df275@EVIL>
2026-04-13 20:57:52 +0500

    Разработанные контексты данных. Чернышков.

---
+++ ShopContent_Chernyshkov/Context/CategorysContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using ShopContent_Chernyshkov.Modell;
+
+namespace ShopContent_Chernyshkov.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategorys()
+        {
+
+            ObservableCollection<CategorysContext> allCategorys = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Connection.Query("SELECT * FROM [dbo].[Categorys]", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new CategorysContext()
+                {
+                    Id = dataCategorys.GetInt32(0),
+                    Name = dataCategorys.GetString(1),
+                });
+            }
+            connection.CloseConnection(connection);
+
+            return allCategorys;
+        }
+    }
+}
