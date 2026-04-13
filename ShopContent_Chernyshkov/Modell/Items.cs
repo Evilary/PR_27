e4ab206cee279c82d26df48545527f47e371dd55
df275 <df275@EVIL>
2026-04-13 20:19:50 +0500

    Разработанные модели данных. Чернышков

---
+++ ShopContent_Chernyshkov/Modell/Items.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShopContent_Chernyshkov.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            get { return Price; }
+            set
+            {
+                Price = value;
+                OnPropertyChanged("Price");
+
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return Description; }
+            set
+            {
+                Description = value;
+                OnPropertyChanged("Description");
+
+            }
+        }
+
+        private Categorys category;
+        public Categorys Category
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Category");
+
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));  
+        }
+    }
+}
