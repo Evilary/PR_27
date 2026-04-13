ca16c548d8cd4c0f62ae364fed0405a8c2ec9a87
df275 <df275@EVIL>
2026-04-13 20:05:33 +0500

    Разработанные классы общего пользования. Чернышков.

---
+++ ShopContent_Chernyshkov/Classes/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopContent_Chernyshkov.Classes
+{
+    public class Connection
+    {
+        private static string ConnectionConnection = "server=127.0.0.1;uid=root;pwd=;database=;";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
