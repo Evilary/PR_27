7e2f1427d37bd04fc1f6203eb461ae9c658baa29
df275 <df275@EVIL>
2026-04-13 19:15:15 +0500

    Разработанное главное окно. Чернышков.

--- ShopContent_Chernyshkov/MainWindow.xaml.cs
+++ ShopContent_Chernyshkov/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ShopContent_Chernyshkov
 {
@@ -20,9 +10,15 @@ namespace ShopContent_Chernyshkov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public View.Main Main = new View.Main();
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(Main);
         }
+        private void OpenIndex(object sender, MouseButtonEventArgs e)=> 
+            frame.Navigate(Main);
     }
 }
